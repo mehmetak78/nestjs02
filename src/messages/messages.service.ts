fb740a7f708de691d93fabfa7b6722cae49349ab
mehmetak <mehmetak78@hotmail.com>
2022-06-20 21:14:39 +0300

    DI

---
+++ src/messages/messages.service.ts
@@ -0,0 +1,21 @@
+import MessagesRepository from "./messages.repository";
+import {Injectable} from "@nestjs/common";
+
+@Injectable()
+export default class MessagesService {
+
+    constructor(public messagesRepo: MessagesRepository) {}
+
+    async findOne(id: string) {
+        return this.messagesRepo.findOne(id);
+    }
+
+    async findAll() {
+        return this.messagesRepo.findAll();
+    }
+
+    async create(content: string) {
+        return this.messagesRepo.create(content);
+    }
+
+}
